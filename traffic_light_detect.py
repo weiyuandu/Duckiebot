fc56f017f3214cb743568dbbe992c1aa3cf69526
ge94caf <ge94caf@tum.de>
2025-08-24 08:53:12 +0800

    traffic light detection and map include traffic light

---
+++ traffic_light_detect.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# manual
+
+"""
+This script allows you to manually control the simulator or Duckiebot
+using the keyboard arrows.
+"""
+
+import sys
+import argparse
+import pyglet
+from pyglet.window import key
+import numpy as np
+import cv2
+import gym
+import gym_duckietown
+from gym_duckietown.envs import DuckietownEnv
+from gym_duckietown.wrappers import UndistortWrapper
+
+# from experiments.utils import save_img
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', default=None)
+parser.add_argument('--map-name', default='small_loop')
+parser.add_argument('--distortion', default=False, action='store_true')
+parser.add_argument('--draw-curve', action='store_true', help='draw the lane following curve')
+parser.add_argument('--draw-bbox', action='store_true', help='draw collision detection bounding boxes')
+parser.add_argument('--domain-rand', action='store_true', help='enable domain randomization')
+parser.add_argument('--frame-skip', default=1, type=int, help='number of frames to skip')
+parser.add_argument('--seed', default=1, type=int, help='seed')
+args = parser.parse_args()
+
+env = DuckietownEnv(
+    seed = args.seed,
+    map_name = args.map_name,
+    draw_curve = args.draw_curve,
+    draw_bbox = args.draw_bbox,
+    domain_rand = args.domain_rand,
+    frame_skip = args.frame_skip,
+    distortion = args.distortion,
+)
+
+
+env.reset()
+env.render()
+
+
+def detect_light(image):
+    h, w, _ = image.shape
+    roi = image[0:int(h/3), int(w/3):int(2*w/3)]
+    hsv = cv2.cvtColor(roi, cv2.COLOR_RGB2HSV)
+
+    lower_red1 = np.array([0, 120, 120])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([160, 120, 120])
+    upper_red2 = np.array([179, 255, 255])
+    lower_green = np.array([40, 120, 120])
+    upper_green = np.array([90, 255, 255])
+    red_mask = cv2.inRange(hsv, lower_red1, upper_red1) | cv2.inRange(hsv, lower_red2, upper_red2)
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    gray = cv2.cvtColor(roi, cv2.COLOR_RGB2GRAY)
+    circles = cv2.HoughCircles( gray, cv2.HOUGH_GRADIENT, dp=1.2, minDist=20, param1=50, param2=15, minRadius=5, maxRadius=30 )
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        h_roi, w_roi = red_mask.shape
+        for (x, y, r) in circles[0, :]:
+            if 0 <= x < w_roi and 0 <= y < h_roi:
+                if red_mask[y, x] > 0:
+                    return "RED"
+                elif green_mask[y,x] > 0:
+                    return "GREEN"
+
+        return None
+
+
+@env.unwrapped.window.event
+def on_key_press(symbol, modifiers):
+    """
+    This handler processes keyboard commands that
+    control the simulation
+    """
+
+    if symbol == key.BACKSPACE or symbol == key.SLASH:
+        print('RESET')
+        env.reset()
+        env.render()
+    elif symbol == key.PAGEUP:
+        env.unwrapped.cam_angle[0] = 0
+    elif symbol == key.ESCAPE:
+        env.close()
+        sys.exit(0)
+
+    # Take a screenshot
+    # UNCOMMENT IF NEEDED - Skimage dependency
+    # elif symbol == key.RETURN:
+    #     print('saving screenshot')
+    #     img = env.render('rgb_array')
+    #     save_img('screenshot.png', img)
+
+# Register a keyboard handler
+key_handler = key.KeyStateHandler()
+env.unwrapped.window.push_handlers(key_handler)
+
+def update(dt):
+    """
+    This function is called at every frame to handle
+    movement/stepping and redrawing
+    """
+
+    action = np.array([0.0, 0.0])
+
+    if key_handler[key.UP]:
+        action = np.array([0.44, 0.0])
+    if key_handler[key.DOWN]:
+        action = np.array([-0.44, 0])
+    if key_handler[key.LEFT]:
+        action = np.array([0.35, +1])
+    if key_handler[key.RIGHT]:
+        action = np.array([0.35, -1])
+    if key_handler[key.SPACE]:
+        action = np.array([0, 0])
+
+    # Speed boost
+    if key_handler[key.LSHIFT]:
+        action *= 1.5
+
+    obs, reward, done, info = env.step(action)
+    print('step_count = %s, reward=%.3f' % (env.unwrapped.step_count, reward))
+
+    light = detect_light(obs)
+    if light:
+        print("Traffic light:", light)
+
+
+    if key_handler[key.RETURN]:
+        from PIL import Image
+        im = Image.fromarray(obs)
+
+        im.save('screen.png')
+
+    #if done:
+    #    print('done!')
+    #    env.reset()
+    #    env.render()
+
+    env.render()
+
+pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
+
+# Enter main event loop
+pyglet.app.run()
+
+env.close()
